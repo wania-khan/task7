aac39ca9da344bd996ebe2bbd718b8ab1d212f7f
Wania Khan <waniak305@gmail.com>
2024-04-03 23:11:02 +0500

    update 1

---
+++ src/components/headerMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import navlogo from '../assets/imgs/navlogo.png';
+
+function headerMenu(){
+    return(
+    <>
+    <div className='header-menu'>
+    <div className='navbar-logo'>
+      <img src={navlogo} alt='erro' className='navlogo'></img>
+    </div>
+    <div className='navbar-menu'>
+        <span className='menu-bar'>Home&nbsp;&nbsp;Company <span className='gt'>&gt;</span>&nbsp;&nbsp;
+        Platform <span className='gt'>&gt;</span>&nbsp;&nbsp;
+        Resources <span className='gt'>&gt;</span></span>
+    </div>
+    <div className='navbar-signup'>
+      <span className='navbar-login'>Login</span>
+      <button className='button-createAcc'>Create Account</button>
+    </div>
+    </div>
+    </>
+    );
+}
+export default headerMenu;
